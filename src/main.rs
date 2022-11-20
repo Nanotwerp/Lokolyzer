84ef89406abb2596eed8335b06f36884131fe9a4
Nanotwerp <Nanotwerp@gmail.com>
2022-11-19 23:32:52 -0500

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,248 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
+use std::path::Path;
+
+pub enum CharType {
+    Letter,
+    Digit,
+    Operator,
+    Unknown,
+}
+pub enum TokenType {
+    IntLit,
+    Ident,
+    AssignOp,
+    AddOp,
+    SubOp,
+    MultOp,
+    DivOp,
+    LeftParen,
+    RightParen,
+}
+
+pub static mut CHAR_CLASS: Vec<u8> = Vec::new();
+pub static mut LEXEME: Vec<char> = Vec::new();
+pub static mut NEXT_CHAR: char = '0';
+pub static mut LEX_LEN: usize = 0;
+pub static mut TOKEN: Vec<u8> = Vec::new();
+pub static mut NEXT_TOKEN: Vec<u8> = Vec::new();
+
+pub static mut LEX_TYPE: Vec<char> = Vec::new();
+
+pub fn main() {
+    let path = Path::new("mathy.txt");
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Could not open {}: {}", display, why),
+        Ok(file) => file,
+    };
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("Could not read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
+
+    addchar().err();
+    lookup();
+    lex();
+}
+
+fn lookup() {
+    unsafe {
+        let mut chartype: char = LEXEME[0];
+        for i in 0..LEXEME.len() - 1 {
+            match chartype {
+                '+' => {
+                    LEX_TYPE.push('+');
+                }
+                '-' => {
+                    LEX_TYPE.push('-');
+                }
+                '*' => {
+                    LEX_TYPE.push('*');
+                }
+                '/' => {
+                    LEX_TYPE.push('/');
+                }
+                '%' => {
+                    LEX_TYPE.push('%');
+                }
+                '=' => {
+                    LEX_TYPE.push('=');
+                }
+                '(' => {
+                    LEX_TYPE.push('(');
+                }
+                ')' => {
+                    LEX_TYPE.push(')');
+                }
+                '0' => {
+                    LEX_TYPE.push('0');
+                }
+                '1' => {
+                    LEX_TYPE.push('0');
+                }
+                '2' => {
+                    LEX_TYPE.push('0');
+                }
+                '3' => {
+                    LEX_TYPE.push('0');
+                }
+                '4' => {
+                    LEX_TYPE.push('0');
+                }
+                '5' => {
+                    LEX_TYPE.push('0');
+                }
+                '6' => {
+                    LEX_TYPE.push('0');
+                }
+                '7' => {
+                    LEX_TYPE.push('0');
+                }
+                '8' => {
+                    LEX_TYPE.push('0');
+                }
+                '9' => {
+                    LEX_TYPE.push('0');
+                }
+                'a' => {
+                    LEX_TYPE.push('a');
+                }
+                'b' => {
+                    LEX_TYPE.push('a');
+                }
+                'c' => {
+                    LEX_TYPE.push('a');
+                }
+                'd' => {
+                    LEX_TYPE.push('a');
+                }
+                'e' => {
+                    LEX_TYPE.push('a');
+                }
+                'f' => {
+                    LEX_TYPE.push('a');
+                }
+                'g' => {
+                    LEX_TYPE.push('a');
+                }
+                'h' => {
+                    LEX_TYPE.push('a');
+                }
+                'i' => {
+                    LEX_TYPE.push('a');
+                }
+                'j' => {
+                    LEX_TYPE.push('a');
+                }
+                'k' => {
+                    LEX_TYPE.push('a');
+                }
+                'l' => {
+                    LEX_TYPE.push('a');
+                }
+                'm' => {
+                    LEX_TYPE.push('a');
+                }
+                'n' => {
+                    LEX_TYPE.push('a');
+                }
+                'o' => {
+                    LEX_TYPE.push('a');
+                }
+                'p' => {
+                    LEX_TYPE.push('a');
+                }
+                'q' => {
+                    LEX_TYPE.push('a');
+                }
+                'r' => {
+                    LEX_TYPE.push('a');
+                }
+                's' => {
+                    LEX_TYPE.push('a');
+                }
+                't' => {
+                    LEX_TYPE.push('a');
+                }
+                'u' => {
+                    LEX_TYPE.push('a');
+                }
+                'v' => {
+                    LEX_TYPE.push('a');
+                }
+                'w' => {
+                    LEX_TYPE.push('a');
+                }
+                'x' => {
+                    LEX_TYPE.push('a');
+                }
+                'y' => {
+                    LEX_TYPE.push('a');
+                }
+                'z' => {
+                    LEX_TYPE.push('a');
+                }
+                ' ' => {
+                    LEX_TYPE.push(' ');
+                }
+                '\n' => {
+                    LEX_TYPE.push('N');
+                    print!("Line!");
+                }
+                '_' => {
+                    LEX_TYPE.push('_');
+                }
+                ';' => {
+                    LEX_TYPE.push(';');
+                }
+                _ => {
+                    LEX_TYPE.push('?');
+                    print!(
+                        "Error! This code has a character that cannot be processed by Lokolyzer."
+                    )
+                }
+            }
+            chartype = LEXEME[i + 1];
+        }
+    }
+}
+
+fn addchar() -> io::Result<()> {
+    unsafe {
+        let f = File::open("mathy.txt")?;
+        let f = BufReader::new(f);
+
+        for line in f.lines() {
+            for c in line?.chars() {
+                LEXEME.push(c);
+            }
+            LEXEME.push('\n');
+        }
+
+        Ok(())
+    }
+}
+
+fn lex() {
+    unsafe {
+        let mut charclass: char = LEX_TYPE[0];
+        let mut lettercount = 0;
+        for i in 0..LEX_TYPE.len() - 1 {
+            match charclass {
+                'a' => {
+                    print!("{}", LEXEME[i]);
+                    lettercount = lettercount + 1;
+                }
+                'N' => {
+                    print!("{}", LEXEME[i]);
+                    println!();
+                }
+                _ => print!("H"),
+            }
+            charclass = LEX_TYPE[i + 1];
+        }
+    }
+}
